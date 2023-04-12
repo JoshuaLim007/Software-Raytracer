fa2ea73a9e53314177a928cea147e1c90c98adfd
Joshua Lim <66184501+JoshuaLim007@users.noreply.github.com>
2023-04-12 14:31:23 -0700

    qos update

--- Raytracer/Common.hpp
+++ Raytracer/Common.hpp
@@ -349,5 +349,19 @@ template<class T>
 T min(T a, T b) {
 	return a < b ? a : b;
 }
+template<class T>
+T smoothstep(T edge0, T edge1, T x)
+{
+	if (x < edge0)
+		return 0;
+
+	if (x >= edge1)
+		return 1;
+
+	// Scale/bias into [0..1] range
+	x = (x - edge0) / (edge1 - edge0);
+
+	return x * x * (3 - 2 * x);
+}
 
 #endif
